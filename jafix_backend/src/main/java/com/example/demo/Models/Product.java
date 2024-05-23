ecdb09e898a5e3538773427326c647e5c6ec2c86
PENGUIN <artursurmin2@gmail.com>
2024-05-23 21:58:22 +0300

    front

--- jafix_backend/src/main/java/com/example/demo/Models/Product.java
+++ jafix_backend/src/main/java/com/example/demo/Models/Product.java
@@ -1,2 +1,49 @@
-package com.example.demo.Models;public class Product {
+package com.example.demo.Models;
+
+import java.util.Map;
+
+public class Product {
+    private Integer id;
+    private Double price;
+    private String name;
+    private Map<String,String> components;
+
+    public Product(Integer id, Double price, String name, Map<String, String> components) {
+        this.id = id;
+        this.price = price;
+        this.name = name;
+        this.components = components;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Map<String, String> getComponents() {
+        return components;
+    }
+
+    public void setComponents(Map<String, String> components) {
+        this.components = components;
+    }
 }
--- jafix_backend/src/main/java/com/example/demo/Models/ProductComponent.java
+++ jafix_backend/src/main/java/com/example/demo/Models/ProductComponent.java
@@ -1,2 +1,57 @@
-package com.example.demo.Models;public class ProductComponent {
+package com.example.demo.Models;
+
+public class ProductComponent {
+    private Integer id;
+    private Integer batch_id;
+    private Integer product_id;
+    private Integer amount;
+    private String ingredient;
+
+    public ProductComponent(Integer id, Integer batch_id, Integer product_id, Integer amount, String ingredient) {
+        this.id = id;
+        this.batch_id = batch_id;
+        this.product_id = product_id;
+        this.amount = amount;
+        this.ingredient = ingredient;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getBatch_id() {
+        return batch_id;
+    }
+
+    public void setBatch_id(Integer batch_id) {
+        this.batch_id = batch_id;
+    }
+
+    public Integer getProduct_id() {
+        return product_id;
+    }
+
+    public void setProduct_id(Integer product_id) {
+        this.product_id = product_id;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+
+    public String getIngredient() {
+        return ingredient;
+    }
+
+    public void setIngredient(String ingredient) {
+        this.ingredient = ingredient;
+    }
 }
